11f046e7210d67dd8dc6b268a3a332de6e01106f
kike12345 <jose12345_14@hotmail.com>
2022-10-11 14:23:45 -0400

    codigo nuevo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-hvdjdqxi.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['ecommerce-env.eba-3mnswnm8.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
